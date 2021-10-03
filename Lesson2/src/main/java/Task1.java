75b2a599863df9ee5491729182ef9656290724e2
Егор <sedunegor123@gmail.com>
2021-10-03 23:08:05 +0300

    исправленное д.з.

--- Lesson2/src/main/java/Task1.java
+++ Lesson2/src/main/java/Task1.java
@@ -10,42 +10,49 @@ public class Task1 {
         System.out.println(sum(100, 200));
         System.out.println(sum(Integer.MAX_VALUE, Integer.MAX_VALUE));
         System.out.println(max(549, 120));
-        System.out.println(average(new int[]{0, -2, 3, -1, 5}));
+        System.out.println(average(new int[]{1, 2, 3, 4, 6}));
         System.out.println(max(new int[]{1, 2, 3, 105, 5, 100, 99}));
         System.out.println(calculateHypotenuse(3, 4));
     }
 
-    public static int sum(int a, int b) {
-        int result = a + b;                         //доп. условие сделаю чуть позже.
+    public static long sum(long a, long b) {
+        long result = a + b;
+        if (result > Integer.MAX_VALUE){     //не придумал другого способа борьбы с арифметическим переполнением int для выполнения доп. условия, кроме как изменить условие задачи.. нужна подсказка.
+            return -1;
+        }
         return result;
     }
 
     public static int max(int a, int b) {
         if (a >= b) {
             return a;
-        } else return b;
+        } else {
+            return b;
+        }
     }
 
     public static double average(int[] array) {
-        int sum = 0;
+        double sum = 0;
         for (int i = 0; i < array.length; i++) {
             sum = sum + array[i];
         }
-        int result = sum / array.length;
+        double result = sum / array.length;
         return result;
     }
 
     public static int max(int[] array) {
-        List<Integer> list = new ArrayList<Integer>();
-        for (int i = 0; i < array.length; i++) {
-            list.add(array[i]);
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
         }
-        return Collections.max(list);
+        return max;
     }
 
     public static double calculateHypotenuse(int a, int b) {
-        double Hypotenuse = Math.sqrt(a * a + b * b);
-        return Hypotenuse;
+        double hypotenuse = Math.sqrt(a * a + b * b);
+        return hypotenuse;
     }
 }
 
