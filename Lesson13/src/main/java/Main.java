8f7acb12c9872e244770fb5e1d406921f1fa8ada
Егор <sedunegor123@gmail.com>
2021-11-21 18:29:04 +0300

    homework lesson 13

---
+++ Lesson13/src/main/java/Main.java
@@ -0,0 +1,20 @@
+import regular_homework.HomeworkWithRegular;
+import strings_homework.HomeworkWithStrings;
+
+/**
+ * Created by Егор on 21.11.2021.
+ */
+public class Main {
+    public static void main(String[] args) {
+        HomeworkWithStrings.middleOfString("Казаки, просмотреть, практика");
+        HomeworkWithRegular.task_2("cab,  ccab, cccab");
+        HomeworkWithRegular.task_3("Versions: Java  5, Java 6, Java   7, Java 8, Java 12.");
+        HomeworkWithRegular.task_4("One two three for раз два one1 two2 123 ");
+        System.out.println(HomeworkWithRegular.task_5("sdfasfsdf11"));
+        System.out.println(HomeworkWithRegular.task_5("апрва123"));
+        System.out.println(HomeworkWithRegular.task_5("jhgjhg888"));
+        System.out.println(HomeworkWithRegular.task_5("scsdcAAAxdfx5"));
+        System.out.println(HomeworkWithRegular.task_5("asdfsdf1111555"));
+        System.out.println(HomeworkWithRegular.task_5("asdfsd_f1111555"));
+    }
+}
